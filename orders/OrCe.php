ccdba2c67ba7aaa72b848deb02fc1bfedbd8944c
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2021-01-10 16:34:51 +0000

    Readd scratchblocks

--- orders/OrCe.php
+++ orders/OrCe.php
@@ -7,3 +7,13 @@
                 document.body.appendChild(OrCeDiv);
         });
 </script>
+<!--Create scratchblocks in case they are used in an order form - line 13: call parser file. line 14: call scratchblock styles file. lines 15-19: set parser settings-->	
+<script src="https://scratchblocks.github.io/js/scratchblocks-v3.4-min.js"></script>	
+<link rel="stylesheet" src="/scratchblocks.css">	
+<script>	
+	scratchblocks.renderMatching('pre.blocks', {	
+		style: 'scratch3', //Optional, defaults to 'scratch2'.	
+		languages: ['en'], //Optional, defaults to ['en'].	
+		inline: false, //Optional, defaults to false.	
+	});	
+</script>
